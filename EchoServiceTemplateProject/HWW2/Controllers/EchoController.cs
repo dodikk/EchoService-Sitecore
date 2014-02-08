e2157e44d069cc8a9832d9c2bafb67518685f947
Oleksandr Dodatko <adk@sitecore.net>
2014-02-08 15:19:23 +0200

    Initial commit

---
+++ EchoServiceTemplateProject/HWW2/Controllers/EchoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HWW2.Controllers
+{
+    public class EchoController : ApiController
+    {
+        public string Get()
+        {
+            return "value Hello world";
+        }
+
+    }
+}
---
+++ EchoServiceTemplateProject/HWW2/InitializeRoutes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HWW2
+{
+  using System.Web.Http;
+  using System.Web.Routing;
+  using Sitecore.Pipelines;
+
+  public class InitializeRoutes : Sitecore.Mvc.Pipelines.Loader.InitializeRoutes
+  {
+    protected override void RegisterRoutes(RouteCollection routes, PipelineArgs args)
+    {
+      routes.MapHttpRoute("hwroute", "hw/echo", new
+      {
+        controller = "Echo",
+        action = "Get"/*,
+        id = RouteParameter.Optional*/
+      });
+    }
+  }
+}
